9982f57aac01d40324cf63aa34e628a58d16fefe
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-11-18 22:20:28 -0300

    Created api service, stripe client side service and implemented redirect to stripe checkout.

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
 import { useSession, signIn } from 'next-auth/client'
+
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
+
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
@@ -8,13 +12,23 @@ interface SubscribeButtonProps {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
   
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session){
       signIn('github')
       return;
     }
 
-    // criação da checkout session no stripe
+    try {
+      const response = await api.post('/subscribe')
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs()
+
+      await stripe.redirectToCheckout({sessionId})
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (
