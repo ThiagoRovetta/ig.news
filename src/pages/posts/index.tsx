c5304c55cbd4ec6070548148ae5497c6485ba6e6
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-11-22 21:35:27 -0300

    Create dynamic post page component.

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,11 +1,23 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Prismic from '@prismicio/client'
 import { RichText } from 'prismic-dom'
 
 import { getPrismicClient } from '../../services/prismic'
 
 import styles from './styles.module.scss'
+
+type Post = {
+  slug: string;
+  title: string;
+  excerpt: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[]
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
@@ -37,17 +49,6 @@ export const getStaticProps: GetStaticProps = async () => {
   }
 }
 
-type Post = {
-  slug: string;
-  title: string;
-  excerpt: string;
-  updatedAt: string;
-}
-
-interface PostProps {
-  posts: Post[]
-}
-
 export default function Posts({ posts }: PostProps) {
   return (
     <>
@@ -59,11 +60,13 @@ export default function Posts({ posts }: PostProps) {
         <div className={styles.posts}>
           {
             posts.map(post => (
-              <a href="#" key={post.slug}>
-                <time>{post.updatedAt}</time>
-                <strong>{post.title}</strong>
-                <p>{post.excerpt}</p>
-              </a>
+              <Link href={`/posts/${post.slug}`} key={post.slug}>
+                <a>
+                  <time>{post.updatedAt}</time>
+                  <strong>{post.title}</strong>
+                  <p>{post.excerpt}</p>
+                </a>
+              </Link>
             ))
           }
         </div>
