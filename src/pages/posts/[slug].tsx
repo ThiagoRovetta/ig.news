98fc494d917f70a3972bba03f3b361b26c860b47
ThiagoRovetta <rovetta.thiago@gmail.com>
2021-11-23 23:18:13 -0300

    Created conditions to redirect user if there's no active subscription.

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -29,15 +29,18 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     }
   }
 
-  // if (!session) {
-
-  // }
+  if (!session.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req);
 
   const response = await prismic.getByUID('post', String(slug), {})
-
-  console.log('response', response);
 
   const post = {
     slug,
